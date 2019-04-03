197e181ddb4f1a7a5c5f480a61377a900aa168e9
Alexandre <aas@unochapeco.edu.br>
2019-04-03 08:52:55 -0300

    ALteração ListaCarrosActivity

--- app/src/main/java/com/s/d/a/a/conceitodashboard/ListaCarrosActivity.java
+++ app/src/main/java/com/s/d/a/a/conceitodashboard/ListaCarrosActivity.java
@@ -1,9 +1,18 @@
 package com.s.d.a.a.conceitodashboard;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.s.d.a.a.servico.CarroService;
+
+
 
 public class ListaCarrosActivity extends AppCompatActivity {
+    private static final String TAG = "ConceitoDashboard";
 
     private String tipo;
 
@@ -12,7 +21,20 @@ public class ListaCarrosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_carros);
 
-        tipo = getIntent().getStringExtra("tipo");
+
+        try{
+            tipo = getIntent().getStringExtra("tipo");
+            List<Carro> carros = CarroService.getCarros(this, tipo);
+            for(Carro c : carros){
+                Log.i(TAG, "Carro: " + c.nome);
+            }
+        }catch (IOException e){
+            Log.e(TAG, e.getMessage(), e);
+        }
 
     }
+
+
+
+
 }
